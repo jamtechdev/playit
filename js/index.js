4a2109ec1a08a40802df3c8f3763651f5bc22e66
G-cake <you@example.com>
2019-12-05 15:29:25 +0530

    Compiled files added

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.calendar').eCalendar({
+
+ weekDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        textArrows: {previous: '<', next: '>'},
+        });
+});
+$(document).ready(function () {
+    $('.calendar-4').eCalendar({
+
+ weekDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        textArrows: {previous: '<', next: '>'},
+        });
+});
+
---
+++ precache-manifest.bf5991b8d0ca78c66c3aac97a6cff9de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c17532ae16474b153d7846a4c03674be",
+    "url": "/static/media/host-profile.c17532ae.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bcacca5cafdb790b3e97",
+    "url": "/static/js/main.9e0a7a56.chunk.js"
+  },
+  {
+    "revision": "ee19fa0693d49d4b4549",
+    "url": "/static/js/2.4ac6bc7d.chunk.js"
+  },
+  {
+    "revision": "bcacca5cafdb790b3e97",
+    "url": "/static/css/main.ffa57fb0.chunk.css"
+  },
+  {
+    "revision": "ee19fa0693d49d4b4549",
+    "url": "/static/css/2.252c7b7e.chunk.css"
+  },
+  {
+    "revision": "85b4beaf44a49fe8d3ec9fe7f3e84a44",
+    "url": "/index.html"
+  }
+];
